1b955edd889d0bc23a8d0c0f5642ab3aa2c11572
Elechia Juma <otienoelechia3@gmail.com>
2024-11-09 01:42:33 +0300

    added changes to app.jsx and added new file

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,25 +1,13 @@
-import UserProfile from './components/UserProfile';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-
-import WelcomeMessage from './components/WelcomeMessage';
-import './app.css'
+import React from 'react';
+import Counter from './components/Counter';
 
 function App() {
   return (
-
-      <div>
-          <WelcomeMessage />
-          <Header />
-          <MainContent />
-          <Footer />
-          <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-      </div>
+    <div className="App">
+      <h1>Counter Application</h1>
+      <Counter />
+    </div>
   );
 }
 
 export default App;
-
-
-
