e19d4cba5f3a302ba03502929f5b53892c8a3ca6
Elechia Juma <otienoelechia3@gmail.com>
2024-11-10 08:47:58 +0300

    added more changes

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', backgroundColor: '#333', color: 'white' }}>
+    <nav style={{display:"flex", justifyContent:"'space-between", padding: '10px', backgroundColor: '#333', color: 'white' }}>
       <Link to="/" style={{ margin: '0 10px', color: 'white' }}>Home</Link>
       <Link to="/about" style={{ margin: '0 10px', color: 'white' }}>About</Link>
       <Link to="/services" style={{ margin: '0 10px', color: 'white' }}>Services</Link>
