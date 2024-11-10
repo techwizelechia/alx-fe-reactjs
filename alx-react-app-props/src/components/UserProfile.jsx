df40ca97e66266d7544e126992cb9c4ab26502a2
Elechia Juma <otienoelechia3@gmail.com>
2024-11-10 08:53:18 +0300

    added more changes

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,1 +1,18 @@
+// src/UserProfile.jsx
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
+  return (
+    <div style={{ padding: '20px', border: '1px solid #ddd', borderRadius: '8px' }}>
+      <h2>User Profile</h2>
+      <p><strong>Name:</strong> {userData.name}</p>
+      <p><strong>Email:</strong> {userData.email}</p>
+    </div>
+  );
+}
+
+export default UserProfile;
 
