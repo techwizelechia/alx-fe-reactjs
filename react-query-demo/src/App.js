b290a4ce37744c34ae05981603a59f444bbf4ee2
Elechia Juma <otienoelechia3@gmail.com>
2024-11-22 22:33:16 +0300

    added files to repo

--- react-query-demo/src/App.js
+++ react-query-demo/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import PostsComponent from './PostsComponent';
+import PostsComponent from './components/PostsComponent';
 
 const queryClient = new QueryClient();
 
