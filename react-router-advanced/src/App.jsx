525e8618d2176f3911eb36567a6f72aa5c45df72
techwizelechia <otienoelechia3@gmail.com>
2024-11-24 11:37:41 +0300

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -12,6 +12,7 @@ function App() {
         <Route path="/profile/*" element={<Profile />} />
         <Route path="/login" element={<Login />} />
         <Route path="/post/:id" element={<Post />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
