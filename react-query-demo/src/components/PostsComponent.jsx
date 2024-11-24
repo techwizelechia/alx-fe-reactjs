2fca1639b7778819516765eb7399f61a9fb15e48
techwizelechia <otienoelechia3@gmail.com>
2024-11-24 11:19:11 +0300

    Update PostsComponent.jsx

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -3,7 +3,7 @@ import { useQuery } from '@tanstack/react-query';
 
 const fetchPosts = async () => {
   const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-  if (!response.ok) throw new Error('Network response was not ok');
+  if (!response.ok) throw new isError('Network response was not ok');
   return response.json();
 };
 
