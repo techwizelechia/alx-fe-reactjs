4aeeb29b254defffeb00762283fdff8a1d2cc395
Elechia Juma <otienoelechia3@gmail.com>
2024-11-25 11:02:24 +0300

    Tailwind installation practice

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  // content: [
+  //   "./index.html",
+  //   "./src/**/*.{js,ts,jsx,tsx}",
+  // ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
