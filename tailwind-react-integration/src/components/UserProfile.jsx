8b8d8d5ac505df2241d5a5e7296db9c774df0b60
Elechia Juma <otienoelechia3@gmail.com>
2024-11-28 04:40:26 +0300

    add styling to card

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,9 +1,9 @@
 
 function UserProfile() {
     return (
-      <div className="bg-gray-100 p-8 sm:max-w-sm max-w-xs mx-auto rounded-lg shadow-lg my-20 sm:p-4 md:p-8 md:max-w-sm">
-        <img className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 w-36 h-36 mx-auto" src="https://via.placeholder.com/150" alt="User" />
-        <h1 className="text-xl sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+      <div className="bg-gray-100 hover:shadow-xl p-8 sm:max-w-sm max-w-xs mx-auto rounded-lg shadow-lg my-20 sm:p-4 md:p-8 md:max-w-sm">
+        <img className="rounded-full hover:scale-110  sm:w-24 sm:h-24 md:w-36 md:h-36 w-36 h-36 mx-auto transition-transform duration-300 ease-in-out" src="https://via.placeholder.com/150" alt="User" />
+        <h1 className="hover:text-blue-500 text-xl sm:text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
         <p className="text-gray-600 text-base sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
       </div>
     );
